4a2681111e76fc7c8b6777a1743a28a2554abc20
André Díaz <andre290800@gmail.com>
2022-03-11 22:11:39 -0600

    feat: typography

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <Container>
-      <Button variant="primary">Hoax Buster</Button>{" "}
-      <Button variant="secondary">Call Now</Button>
+      <h1>Fight Virus With Vaccines</h1>
+      <p>Let's stop this pandemic by killing the virus with a vaccine, don't let yourself and your family get infected</p>
+      <h2>Why should I vaccine?</h2>
+      <p>In order to avoid any boubts about getting the COVID-19 vaccine, identify the following 4 benefits of COVID-19 vaccination:</p>
     </Container>
   );
 }
