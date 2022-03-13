6bd9c59c130cfcf7bf52445a6f5c62cdfab97bd6
André Díaz <andre290800@gmail.com>
2022-03-12 23:27:07 -0600

    feat: icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShield } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <Container>
-      <h1>Fight Virus With Vaccines</h1>
-      <p>Let's stop this pandemic by killing the virus with a vaccine, don't let yourself and your family get infected</p>
-      <h2>Why should I vaccine?</h2>
-      <p>In order to avoid any boubts about getting the COVID-19 vaccine, identify the following 4 benefits of COVID-19 vaccination:</p>
+      <span className="icon-container icon-container-accent">
+        <FontAwesomeIcon icon={faShield} className="icon-accent" />
+      </span>
+      <span className="icon-container icon-container-2xl">
+        <FontAwesomeIcon icon={faShield} size="2xl" className="icon-primary" />
+      </span>
     </Container>
   );
 }
