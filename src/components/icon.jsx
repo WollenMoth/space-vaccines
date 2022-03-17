ccbf48f68b443ba42a02463d17b2cba30a44b518
André Díaz <andre290800@gmail.com>
2022-03-16 18:27:53 -0600

    style: change icon sizes

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Icon({ icon, variant = "icon-primary", size = "sm" }) {
+function Icon({ icon, variant = "icon-primary", size = null }) {
   return (
     <span
       className={
         "icon-container" +
         ((variant === "icon-accent" && " icon-container-accent") || "") +
-        ((size === "xl" && " icon-container-xl") || "")
+        ((size === "2xl" && " icon-container-2xl") || "")
       }
     >
       <FontAwesomeIcon icon={icon} size={size} className={variant} />
