4cf71b36eed6921da5d6e01dfb5ffdc2fe2afe87
André Díaz <andre290800@gmail.com>
2022-03-20 21:12:15 -0600

    feat: benefits

---
+++ src/components/benefits.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import BenefitCards from "./benefitCards";
+
+function Benefits(props) {
+  return (
+    <div className="grid">
+      <div className="grid-content">
+        <h2>Why I should vaccine?</h2>
+        <p>
+          In order to avoid any doubts about getting the COVID-19 vaccine,
+          identify the following 4 benefits of COVID-19 vaccination:
+        </p>
+      </div>
+      <div className="grid-bottom">
+        <BenefitCards />
+      </div>
+    </div>
+  );
+}
+
+export default Benefits;
